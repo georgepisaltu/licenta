42c105cea0c4a96ef9f1a9f7acb73f812999a44f
George Pisaltu <gplgit@gmail.com>
2020-06-01 15:29:50 +0300

    Update 3

---
+++ micro_http/src/client.rs
@@ -0,0 +1,33 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+use common::message::Message;
+use common::{ClientError, MessageError};
+use common::Version;
+use request::Request;
+use response::Response;
+use response::StatusCode;
+
+pub struct Client {
+    socket: TcpStream,
+    addr: String,
+    buffer: Vec<u8>,
+}
+
+impl Client {
+    pub fn new(url: String, buffer_size: usize) -> Result<Client, ClientError> {
+        let stream = TcpStream::connect(url.clone()).map_err(ClientError::StreamError)?;
+        Ok(Client {
+            socket: stream,
+            addr: url,
+            buffer: Vec::with_capacity(buffer_size),
+        })
+    }
+
+    pub fn request(&mut self, mut request: Request) -> Result<Response, MessageError> {
+        request
+            .send(&mut self.socket)
+            .map_err(|_| MessageError::IOError)?;
+        Ok(Response::receive(&mut self.socket)?)
+    }
+}
